520c904eb5eef722c36666456328d9d86a2c91e4
Premansh Sharma <Premanshsharma97@gmail.com>
2025-08-17 14:04:53 +0530

    Add files via upload
    Java Developer Internship Task 1 "Hello World".

---
+++ Task1_HelloWorld.java
@@ -0,0 +1,7 @@
+// Task1_HelloWorld.java
+public class Task1_HelloWorld {
+    public static void main(String[] args) {
+        // Printing message to console
+        System.out.println("Hello, World!");
+    }
+}
